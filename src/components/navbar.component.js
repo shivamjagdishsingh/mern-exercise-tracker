9a366d60da66b6272bd6770b960ae83b8615fe45
shivam <shivam.singh@nisevatech.in>
2020-06-12 23:30:08 +0530

    minor frontend change

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return(
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-            <Link to="/" className="navbar-brand">ExcerTracker</Link>
+            <Link to="/" className="navbar-brand">Excercise Tracker</Link>
             <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
